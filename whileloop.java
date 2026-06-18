5023b162fb7f00c7c92ab4dcebdaeec93f3851fb
Mayur Vijay Sawarbandhe <mayursawarbandhe4@gmail.com>
2026-06-18 22:21:29 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,127 @@
+//PRINT REVERSE OF THE NUMBER
+
+// public class whileloop {
+
+//     public static void main(String[] args) {
+//         int n=10899;
+//         while (n>0) {
+//             int lastDigit = n % 10;
+//             System.out.print(lastDigit);
+//             n= n/10; 
+//         }
+//         System.out.println();
+//     }
+// }
+
+
+//SQUARE PATTERN
+
+// public class whileloop {
+
+//     public static void main(String[] args) 
+//     {
+//         int i=1;
+//         while (i<=4) {
+//             System.out.println("****");
+//             i++;
+//         }
+//     }
+// }
+
+
+//PRINT THE SUM OF N NATURAL NUMBER
+
+// import java.util.Scanner;
+
+// public class loops {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         int sum =0;    
+//         int i=1;
+//         while (i <= n) {
+//             sum = sum + i;
+//             System.out.println(sum);
+//             i++;    
+//         }
+//     }
+// }
+
+
+// PRINT NUMBER 1 TO N
+
+// import java.util.Scanner;
+
+// public class loops {
+
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int range = sc.nextInt();
+//         int i = 1;
+//         while (i <= range) {
+//             System.out.println(i);
+//             i++;
+            
+//         }
+//     }
+// }
+
+
+//PRINT THE NUMBER 1 TO 10
+
+// public class loops {
+
+//     public static void main(String[] args) {
+//         int i=1;
+//         int n=7;
+//         while (i<=n) {
+//             // System.out.print(i + " ");
+//             System.out.println(i);
+//             i++;
+//         }
+//     }
+// }
+
+
+//DO WHILE LOOP
+// public class loops {
+
+//     public static void main(String[] args) {
+//         int count=0;
+//         do {
+//             System.out.println(count+"Hello World");
+//             count++;
+//         }
+//         while (count <= 100);
+//     }
+// }
+
+
+//WHILE LOOP
+// public class loops {
+
+//     public static void main(String[] args) {
+//         int counter=0;
+//         while (counter <= 100) {
+//             System.out.println(counter+"Hello World");
+//             counter++;
+//         }
+//     }
+// }
+
+
+
+//FOR LOOPS
+// public class loops {
+
+//     public static void main(String[] args) {
+//         //  a="Hello World";
+//         for(int a=1; a<=100; a++){
+//             System.out.println(a+"Hello World");
+//         }
+//     }
+// }
+
+
+
